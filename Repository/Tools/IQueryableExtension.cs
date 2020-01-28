a96861a993edea6f2e3ce1d5fa26e603b30ae16c
Aitenbichler Herbert <h.aitenbichler@eduhi.at>
2020-01-28 11:26:25 +0100

    PR 159: GetSql for IQueryable (from GitHub)

--- Repository/Tools/IQueryableExtension.cs
+++ Repository/Tools/IQueryableExtension.cs
@@ -14,44 +14,36 @@
   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 */
 
-using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
 
-using Microsoft.EntityFrameworkCore.Query.Internal;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
-using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+// see: https://stackoverflow.com/questions/37527783/get-sql-code-from-an-entity-framework-core-iqueryablet
 
 namespace Framework.Repository.Tools
 {
-/*
     public static class IQueryableExtensions
     {
-        private static readonly TypeInfo QueryCompilerTypeInfo = typeof(QueryCompiler).GetTypeInfo();
-
-        private static readonly FieldInfo QueryCompilerField = typeof(EntityQueryProvider).GetTypeInfo().DeclaredFields.First(x => x.Name == "_queryCompiler");
-
-        private static readonly FieldInfo QueryModelGeneratorField = QueryCompilerTypeInfo.DeclaredFields.First(x => x.Name == "_queryModelGenerator");
-
-        private static readonly FieldInfo DataBaseField = QueryCompilerTypeInfo.DeclaredFields.Single(x => x.Name == "_database");
-
-        private static readonly PropertyInfo DatabaseDependenciesField = typeof(Database).GetTypeInfo().DeclaredProperties.Single(x => x.Name == "Dependencies");
-
         public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
         {
-            var queryCompiler           = (QueryCompiler)QueryCompilerField.GetValue(query.Provider);
-            var modelGenerator          = (QueryModelGenerator)QueryModelGeneratorField.GetValue(queryCompiler);
-            var queryModel              = modelGenerator.ParseQuery(query.Expression);
-            var database                = (IDatabase)DataBaseField.GetValue(queryCompiler);
-            var databaseDependencies    = (DatabaseDependencies)DatabaseDependenciesField.GetValue(database);
-            var queryCompilationContext = databaseDependencies.QueryCompilationContextFactory.Create(false);
-            var modelVisitor            = (RelationalQueryModelVisitor)queryCompilationContext.CreateQueryModelVisitor();
-            modelVisitor.CreateQueryExecutor<TEntity>(queryModel);
-            var sql = modelVisitor.Queries.First().ToString();
+            using (var enumerator = query.Provider.Execute<IEnumerable<TEntity>>(query.Expression).GetEnumerator())
+            {
+                var relationalCommandCache = enumerator.Private("_relationalCommandCache");
+                var selectExpression       = relationalCommandCache.Private<SelectExpression>("_selectExpression");
+                var factory                = relationalCommandCache.Private<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
 
-            return sql;
+                var sqlGenerator = factory.Create();
+                var command      = sqlGenerator.GetCommand(selectExpression);
+
+                string sql = command.CommandText;
+                return sql;
+            }
         }
+
+        private static object Private(this    object obj, string privateField) => obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+        private static T      Private<T>(this object obj, string privateField) => (T)obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
     }
-*/
 }

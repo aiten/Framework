9e4dc68580caefc1be7be81a1188e4f025fd4124
Aitenbichler Herbert <h.aitenbichler@eduhi.at>
2020-03-13 22:09:27 +0100

    add Pdkdf2 password provider
    add Pdkdf2 password provider

---
+++ MyUnitTest/Tools/PasswordTest.cs
@@ -0,0 +1,52 @@
+﻿/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) Herbert Aitenbichler
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+  to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
+using System.Linq;
+
+using FluentAssertions;
+
+using Framework.Tools;
+using Framework.Tools.Password;
+
+using Xunit;
+
+namespace Framework.MyUnitTest.Tools
+{
+    public class PasswordTest
+    {
+        [Fact]
+        public void Pbkdf2PasswordTest()
+        {
+            var password1 = "MySecretPassword1";
+            var password2 = "MySecretPassword2";
+            var password3 = "Serial.Server";
+
+            var hashProvider = new Pbkdf2PasswordProvider();
+
+            var hash1 = hashProvider.GetPasswordHash(password1);
+            var hash2 = hashProvider.GetPasswordHash(password2);
+            var hash3 = hashProvider.GetPasswordHash(password3);
+
+            hash1.Should().NotContain(password1);
+            hash1.Length.Should().BeGreaterThan(1);
+
+            hashProvider.ValidatePassword(password1, hash1).Should().BeTrue();
+            hashProvider.ValidatePassword(password2, hash2).Should().BeTrue();
+            hashProvider.ValidatePassword(password2, hash1).Should().BeFalse();
+            hashProvider.ValidatePassword(password1, hash2).Should().BeFalse();
+        }
+    }
+}
---
+++ Tools/Password/Pbkdf2PasswordProvider.cs
@@ -0,0 +1,73 @@
+﻿/*
+  This file is part of CNCLib - A library for stepper motors.
+
+  Copyright (c) Herbert Aitenbichler
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+  to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
+
+using System;
+using System.Security.Cryptography;
+
+namespace Framework.Tools.Password
+{
+    public class Pbkdf2PasswordProvider : IPasswordProvider
+    {
+        public const int SaltByteSize     = 24;
+        public const int HashByteSize     = 20; // to match the size of the PBKDF2-HMAC-SHA-1 hash 
+        public const int Pbkdf2Iterations = 1000;
+        public const int IterationIndex   = 0;
+        public const int SaltIndex        = 1;
+        public const int Pbkdf2Index      = 2;
+
+        public string GetPasswordHash(string password)
+        {
+            var    cryptoProvider = new RNGCryptoServiceProvider();
+            byte[] salt           = new byte[SaltByteSize];
+            cryptoProvider.GetBytes(salt);
+
+            var hash = GetPbkdf2Bytes(password, salt, Pbkdf2Iterations, HashByteSize);
+            return Pbkdf2Iterations + ":" +
+                   Convert.ToBase64String(salt) + ":" +
+                   Convert.ToBase64String(hash);
+        }
+
+        public bool ValidatePassword(string password, string correctHash)
+        {
+            char[] delimiter  = { ':' };
+            var    split      = correctHash.Split(delimiter);
+            var    iterations = Int32.Parse(split[IterationIndex]);
+            var    salt       = Convert.FromBase64String(split[SaltIndex]);
+            var    hash       = Convert.FromBase64String(split[Pbkdf2Index]);
+
+            var testHash = GetPbkdf2Bytes(password, salt, iterations, hash.Length);
+            return SlowEquals(hash, testHash);
+        }
+
+        private static bool SlowEquals(byte[] a, byte[] b)
+        {
+            var diff = (uint)a.Length ^ (uint)b.Length;
+            for (int i = 0; i < a.Length && i < b.Length; i++)
+            {
+                diff |= (uint)(a[i] ^ b[i]);
+            }
+            return diff == 0;
+        }
+
+        private static byte[] GetPbkdf2Bytes(string password, byte[] salt, int iterations, int outputBytes)
+        {
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt);
+            pbkdf2.IterationCount = iterations;
+            return pbkdf2.GetBytes(outputBytes);
+        }
+    }
+}

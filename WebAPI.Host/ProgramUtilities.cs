bb02edb49a3e4c5815ec8b8ee1327c148f61afeb
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2020-11-18 08:24:17 +0100

    revert

--- WebAPI.Host/ProgramUtilities.cs
+++ WebAPI.Host/ProgramUtilities.cs
@@ -14,14 +14,13 @@
   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 */
 
-using Microsoft.Extensions.Hosting.WindowsServices;
-
 namespace Framework.WebAPI.Host
 {
     using System;
     using System.IO;
     using System.Reflection;
 
+    using Microsoft.Extensions.Hosting.WindowsServices;
     using Microsoft.Extensions.Hosting;
 
     using NLog;
